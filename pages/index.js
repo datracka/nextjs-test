5264d88552e4b4a455e5c7eb58c0ad584b897cf3
Vicens Fayos <datracka@gmail.com>
2019-01-30 14:58:43 +0100

    add header

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,27 @@
-export default () => <div>Welcome to next.js!</div>;
+import Head from "../components/head";
+
+export default () => (
+  <React.Fragment>
+    <Head />
+    Hello world
+    <p>jsx scoped!</p>
+    <style jsx>{`
+      p {
+        color: blue;
+      }
+      div {
+        background: red;
+      }
+      @media (max-width: 600px) {
+        div {
+          background: blue;
+        }
+      }
+    `}</style>
+    <style global jsx>{`
+      body {
+        background: white;
+      }
+    `}</style>
+  </React.Fragment>
+);
